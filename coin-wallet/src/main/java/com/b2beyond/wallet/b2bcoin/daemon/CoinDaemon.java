61728c4b4345ca91cb31b1c6cea7b1c89e22b979
oliviersinnaeve <Great2013>
2017-09-23 12:27:06 +0200

    windows correct start

--- coin-wallet/src/main/java/com/b2beyond/wallet/b2bcoin/daemon/CoinDaemon.java
+++ coin-wallet/src/main/java/com/b2beyond/wallet/b2bcoin/daemon/CoinDaemon.java
@@ -39,8 +39,8 @@ public class CoinDaemon implements Daemon {
                 ProcessBuilder pb = new ProcessBuilder("binaries/" + daemonExecutable, "--config-file", "configs/b2bcoin.conf",
                         "--log-file", userHome + daemonProperties.getProperty("log-file-coin"));
                 if (operatingSystem.equalsIgnoreCase(B2BUtil.WINDOWS)) {
-                    pb = new ProcessBuilder("cmd", "/c", "start", "", "\"" + location + "\\binaries\\" + daemonExecutable, "--config-file", location + "\\configs\\b2bcoin.conf",
-                            "--log-file", userHome + daemonProperties.getProperty("log-file-coin") + "\"");
+                    pb = new ProcessBuilder("cmd", "/c", "start", "", "\"" + location + "\\binaries\\" + daemonExecutable + "\"", "--config-file", "\"" + location + "\\configs\\b2bcoin.conf\"",
+                            "--log-file", userHome + daemonProperties.getProperty("log-file-coin"));
                 } else {
                     pb.directory(new File(location));
                 }
