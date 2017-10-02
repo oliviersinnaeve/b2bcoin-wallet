4aba043ef42db2829111879a74bce4d2b518d8f7
oliviersinnaeve <Great2013>
2017-10-02 13:22:49 +0200

    Windows trying to get wallet accessible after start - no wallet restart on reset - no need. Trying the sleep after first

--- coin-wallet/src/main/java/com/b2beyond/wallet/b2bcoin/daemon/WalletDaemon.java
+++ coin-wallet/src/main/java/com/b2beyond/wallet/b2bcoin/daemon/WalletDaemon.java
@@ -139,7 +139,7 @@ class WalletDaemonRunnable implements Daemon, Runnable {
             ProcessBuilder pb = new ProcessBuilder(binariesLocation + daemonExecutable, "--config", configLocation + "coin-wallet.conf",
                     "--log-file", userHome + daemonProperties.getString("log-file-wallet"), "--server-root", userHome, "-d");
             if (operatingSystem.equalsIgnoreCase(B2BUtil.WINDOWS)) {
-                pb = new ProcessBuilder(binariesLocation + daemonExecutable, "--config", configLocation + "coin-wallet.conf",
+                pb = new ProcessBuilder("cmd", "/c", "start", binariesLocation + daemonExecutable, "--config", configLocation + "coin-wallet.conf",
                         "--log-file", logLocation + daemonProperties.getString("log-file-wallet"), "--server-root", userHome);
             }
 
