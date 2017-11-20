041f21fd1f55262a4f485e07992dcb10da9dfca9
oliviersinnaeve <oliviersinnaeve@java-developer.be>
2017-11-20 22:50:39 +0100

    Payments on windows ...

--- coin-wallet/src/main/java/com/b2beyond/wallet/b2bcoin/util/CoinUtil.java
+++ coin-wallet/src/main/java/com/b2beyond/wallet/b2bcoin/util/CoinUtil.java
@@ -1,17 +1,22 @@
 package com.b2beyond.wallet.b2bcoin.util;
 
+import org.apache.log4j.Logger;
+
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 
 
 public class CoinUtil {
 
+    private static Logger LOGGER = Logger.getLogger(CoinUtil.class);
+
     public static BigDecimal DIVIDE_BY = new BigDecimal("1000000000000");
 
     public static long getLongForText(String text) {
+        LOGGER.info("Converting " + text + " text to long : " + text);
         BigDecimal decimal = new BigDecimal(text);
         decimal = decimal.multiply(DIVIDE_BY);
-
+        LOGGER.info("Converted " + text + " to long : " + decimal.longValue());
         return decimal.longValue();
     }
 
